06c2d3cf402c95f511f22ade20a42968c5414d67
Jessica <jeshbrand@gmail.com>
2021-03-22 18:22:14 +0000

    made student list

---
+++ directory.rb
@@ -0,0 +1,17 @@
+# first, let's print the list of students
+puts 'The students of Villains Academy'
+puts '--------------------------------'
+puts 'Dr Hannibal Lecter'
+puts 'Darth Vader'
+puts 'Nurse Ratched'
+puts 'Michael Corleone'
+puts 'Alex DeLarge'
+puts 'The Wicked Witch of the West'
+puts 'The Terminator'
+puts 'Freddy Krueger'
+puts 'The Joker'
+puts 'Joffrey Baratheon'
+puts 'Norman Bates'
+print "Overall we have "
+print 11
+puts " students"
