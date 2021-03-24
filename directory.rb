b2247ae251f7cbd446d60d8ac6b382effa2e1e1a
Jessica <jeshbrand@gmail.com>
2021-03-24 18:21:08 +0000

    more refactors  - @students now a global variable.

--- directory.rb
+++ directory.rb
@@ -1,19 +1,19 @@
 # students: Cruella De Vil, Hannibal Lecter, Hamburglar.
-
+@students = []
 def print_header
   puts "The students of Villains Academy".center(35)
   puts "--------------------------------".center(35)
 end
 
-def print(students)
-  if students.count == 1 && students[0][:name] == 'UNNAMED'
+def print_students_list
+  if @students.count == 1 && @students[0][:name] == 'UNNAMED'
     puts "No students to print"
   else
     ctr = 0
-    while ctr < students.count
+    while ctr < @students.count
       # still choosing to index at 1.
-      puts " #{(ctr)+1}: #{students[ctr][:name]} (#{students[ctr][:cohort]} cohort). 
-      Hobby: #{students[ctr][:hobby]}. Nationality: #{students[ctr][:nationality]}".center(50)
+      puts " #{(ctr)+1}: #{@students[ctr][:name]} (#{@students[ctr][:cohort]} cohort). 
+      Hobby: #{@students[ctr][:hobby]}. Nationality: #{@students[ctr][:nationality]}".center(50)
       ctr +=1
     end 
   end
@@ -35,31 +35,31 @@ def validate_cohort_choice()
   end
 end
 
-def print_by_cohort(students)
+def print_by_cohort
 # this should take in a list of all existing students, 
 # and print only the students of a selected cohort.
   puts "Which cohort would you like to print? \n (type a month, or press ENTER for UNASSIGNED)"
   cohort_to_print = validate_cohort_choice()
   puts "Students in the #{cohort_to_print} cohort:"
-  students.each do |student| 
+  @students.each do |student| 
     if student[:cohort] == cohort_to_print.to_sym 
       puts "#{student[:name]}. Hobby: #{student[:hobby]}, Nationality: #{student[:nationality]}"
     end
   end
 end
 
-def print_footer(arr)
-  if arr.count == 1 && arr[0][:name] == 'UNNAMED'
+def print_footer
+  if @students.count == 1 && @students[0][:name] == 'UNNAMED'
     puts "We have no diabolical students :( "
   else
-  arr.count <= 1 ? (puts "Overall we have #{arr.count} diabolical student".center(40)) : (puts "Overall we have #{arr.count} diabolical students".center(40))
+  @students.count <= 1 ? (puts "Overall we have #{@students.count} diabolical student".center(40)) : (puts "Overall we have #{@students.count} diabolical students".center(40))
   end
 end
 
 # this creates the array of students
 def input_students
   puts "Let's add some student info."
-  students = []
+  # students = [] # we made this global in line 1
   finished_listing = false
   
   # this is to handle blank values
@@ -80,36 +80,46 @@ def input_students
     s_hobby = default(gets.strip.capitalize, "UNKNOWN") # helper func
     puts "Student nationality?"
     s_nationality = default(gets.strip.capitalize, "UNKNOWN") # helper func
-    students << {name: s_name, cohort: s_cohort.to_sym, hobby: s_hobby, nationality: s_nationality}
-    students.count <= 1 ? (puts "Now we have #{students.count} student") : (puts "Now we have #{students.count} students")
+    @students << {name: s_name, cohort: s_cohort.to_sym, hobby: s_hobby, nationality: s_nationality}
+    @students.count <= 1 ? (puts "Now we have #{@students.count} student") : (puts "Now we have #{@students.count} students")
     puts "Finished adding students? if so, press 'y' - if not, press any other key."
     finished = gets.strip.downcase
     if finished == 'y'
       finished_listing = true
     end
   end
-  students
+  # students # this is now a global variable
 end
 
-def interactive_menu
-  students = []
-  loop do
+def print_menu
     puts "1. input students"
     puts "2. display all students"
     puts "9. exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "invalid input, try again"
-    end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Invalid input, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
