bb7207a3e45411e2fad2493207a267b46786a35c
Jessica <jeshbrand@gmail.com>
2021-03-25 12:59:10 +0000

    12. Added 'load students. Used .gitignore on command line.

--- directory.rb
+++ directory.rb
@@ -101,10 +101,20 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort, hobby, nationality = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, nationality: nationality}
+  end
+  file.close
+end
+
 def print_menu
     puts "1. input students"
     puts "2. display all students"
     puts "3. save the list of students to students.csv"
+    puts "4. load the list of students from students.csv"
     puts "9. exit"
 end
 
@@ -122,6 +132,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
